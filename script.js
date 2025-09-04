11d3b0807e449843f83447aa769ef31034700def
shoto-87 <shiyoto_nishimura@mail.toyota.co.jp>
2025-09-04 13:13:17 +0900

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+console.log("Hello world!");
+
+// "https://developer.mozilla.org/ja/" MDN
+
+
+
+
+
+
+function iventgator(){
+  const innerTextOutput = document.getElementById("bluebox");
+   innerTextOutput.textContent = "まだできてません";
+}
+
+
+const target = document.getElementById("startbutton");
+target.addEventListener("click",iventgator);
